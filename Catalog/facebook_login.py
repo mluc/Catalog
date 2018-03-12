f445515f0ffde06d58893149f9d9571a04bb7871
My Luc <lucphumy@gmail.com>
2018-03-11 19:11:26 -0500

    add fb login

---
+++ Catalog/facebook_login.py
@@ -0,0 +1,65 @@
+import json
+import httplib2
+from flask import Blueprint
+from flask import request, make_response, flash, session as login_session
+
+facebook_api = Blueprint('facebook_api', __name__)
+
+
+@facebook_api.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data.decode("utf-8")
+    # print("access token received %s " % access_token)
+
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # strip expire tag from access token
+    # token = result.split("&")[0]
+
+    result = result.decode("utf-8")
+    # Use token to get user info from API
+    token = result.split(',')[0].split(':')[1].replace('"', '')
+
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
+    data = json.loads(result.decode("utf-8"))
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout
+    login_session['access_token'] = token
+    login_session['user_id'] = login_session['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
+
+
+@facebook_api.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    # The access token must me included to successfully logout
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "you have been logged out"
